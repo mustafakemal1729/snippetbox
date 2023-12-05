4decc497bfb55dde6ecbf6fa90e02dbaaab3e49f
mustafakemal1729 <experimus1@gmail.com>
2023-12-05 17:31:27 +0100

    configuration done

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,16 +2,15 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"strconv"
 	"text/template"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		app.notFound(w) // Use the notFound() helper
 		return
 	}
 	// Use the template.ParseFiles() function to read the template file into a
@@ -25,32 +24,30 @@ func home(w http.ResponseWriter, r *http.Request) {
 	}
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		log.Print(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+		app.serverError(w, err)
 		return
 	}
 
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-		log.Print(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+		app.serverError(w, err)
 	}
 }
 
-func snippetView(w http.ResponseWriter, r *http.Request) {
+func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
 
 	fmt.Fprintf(w, "Display specific snippet with id %d", id)
 }
 
-func createSnippet(w http.ResponseWriter, r *http.Request) {
+func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", "POST")
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
 
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,15 @@ import (
 	"os"
 )
 
+// Define an application struct to hold the application-wide dependencies
+// for the web application. For now we'll only include fields for the
+// two custom loggers, but we'll add more to it as the build progresses.
+
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
+
 func main() {
 
 	addr := flag.String("addr", ":4000", "HTTP network address")
@@ -23,24 +32,27 @@ func main() {
 	// the destination and use the log.Lshortfile flag to include the relevant file name and line number.
 	errorLog := log.New(os.Stderr, "Error\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	mux := http.NewServeMux()
-	// Create a file server which serves files out of the "./ui/static" directory.
-	// Note that the path given to the http.Dir function is relative to the project // directory root.
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
 
-	// Use the mux.Handle() function to register the file server as the handler
-	// for all URL paths that start with "/static/". For matching paths, we strip the
-	// "/static" prefix before the request reaches the file server.
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	// Register the other application routes as normal.
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", createSnippet)
+	// Initialize a new http.Server struct.
+	// We set the Addr and Handler fields so
+	// that the server uses the same network address and routes as before,
+	// and set the ErrorLog field so that the server now uses the custom
+	// errorLog logger in the event of any problems.
+	server := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		// Call the new app.routes() method to get the servemux containing our routes.
+		Handler: app.routes(),
+	}
 
-	infoLog.Print("Starting server on: ", *addr) // information message
+	infoLog.Printf("Starting server on %s", *addr)
 
 	// The value returned from the flag.String() function is a pointer to the flag value,
 	// not the value itself. So we need to dereference the pointer before using it.
-	err := http.ListenAndServe(*addr, mux)
+	err := server.ListenAndServe()
 	errorLog.Fatal(err)
 }
