79fa9b815c94aa57ed32c4fb5a10a7cba633a848
mustafakemal1729 <experimus1@gmail.com>
2023-12-13 17:18:10 +0100

    custom template function added

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -17,6 +17,21 @@ type templateData struct {
 	Snippets    []*models.Snippet
 }
 
+// Create a humanDate function which returns a nicely formatted string
+// representation of a time.Time object.
+
+func humanDate(t time.Time) string {
+	return t.Format("02 Jan 2006 at 15:04")
+}
+
+// Initialize a template.FuncMap object and store it in a global variable.
+// This is essentially a string-keyed map which acts as a lookup between
+// the names of our custom template functions and the functions themselves.
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
+}
+
 func newTemplateCache() (map[string]*template.Template, error) {
 	cache := map[string]*template.Template{}
 	pages, err := filepath.Glob("./ui/html/pages/*.tmpl")
@@ -26,7 +41,7 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	for _, page := range pages {
 		name := filepath.Base(page)
 		// Parse the base template file into a template set.
-		ts, err := template.ParseFiles("./ui/html/base.tmpl")
+		ts, err := template.New(name).Funcs(functions).ParseFiles("./ui/html/base.tmpl")
 		if err != nil {
 			return nil, err
 		}
