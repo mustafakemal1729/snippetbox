b50fdd2041f4c0ca61c06e04b3d85d52822c478b
mustafakemal1729 <experimus1@gmail.com>
2024-01-06 00:08:52 +0100

    refactor handler

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -5,12 +5,13 @@ import (
 	"fmt"
 	"net/http"
 	"snippetbox/internal/models"
-	"snippetbox/internal/validator" // new import
+	"snippetbox/internal/validator"
 	"strconv"
 
 	"github.com/julienschmidt/httprouter"
 )
 
+// snippetCreateForm holds the form data for creating a new snippet.
 type snippetCreateForm struct {
 	Title               string `form:"title"`
 	Content             string `form:"content"`
@@ -18,11 +19,35 @@ type snippetCreateForm struct {
 	validator.Validator `form:"-"`
 }
 
-func (app *application) home(w http.ResponseWriter, r *http.Request) {
+func (form *snippetCreateForm) Validate() {
+	maxCharLength := 50
 
+	form.CheckField(validator.NotBlank(form.Title), "title", "This field cannot be blank.")
+	form.CheckField(validator.MaxChars(form.Title, maxCharLength), "title", "This field cannot be more than 50 char long.")
+	form.CheckField(validator.NotBlank(form.Content), "content", "This field cannot be blank.")
+	form.CheckField(validator.PermittedInt(form.Expires, 1, 7, 365), "expires", "This field must equal to 1, 7 or 365.")
+}
+
+type appHandler func(http.ResponseWriter, *http.Request) error
+
+// handle is a wrapper that simplifies error handling for HTTP handlers.
+func (fn appHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := fn(w, r)
+	if err != nil {
+		switch {
+		case errors.Is(err, models.ErrNoRecord):
+			http.NotFound(w, r)
+		default:
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		}
+	}
+}
+
+// home handles the home page.
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	snippets, err := app.snippets.Latest()
 	if err != nil {
-		app.serverError(w, err)
+		app.clientError(w, http.StatusBadRequest)
 		return
 	}
 
@@ -30,22 +55,19 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	data.Snippets = snippets
 
 	app.render(w, http.StatusOK, "home.tmpl", data)
+
 }
+
+// snippetView handles viewing a single snippet.
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	params := httprouter.ParamsFromContext(r.Context())
 	id, err := strconv.Atoi(params.ByName("id"))
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
-		return
+		errors.New("invalid ID parameter")
 	}
 
 	snippet, err := app.snippets.Get(id)
 	if err != nil {
-		if errors.Is(err, models.ErrNoRecord) {
-			http.NotFound(w, r)
-		} else {
-			app.serverError(w, err)
-		}
 		return
 	}
 
@@ -53,20 +75,18 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	data.Snippet = snippet
 
 	app.render(w, http.StatusOK, "view.tmpl", data)
-
 }
 
+// snippetCreate handles rendering the snippet creation form.
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	data := app.newTemplateData(r)
-
 	data.Form = snippetCreateForm{Expires: 365}
 
 	app.render(w, http.StatusOK, "form.tmpl", data)
 }
 
+// snippetCreateNote handles creating a new snippet.
 func (app *application) snippetCreateNote(w http.ResponseWriter, r *http.Request) {
-
-	// Declare a new empty instance of the snippetCreateForm struct.
 	var form snippetCreateForm
 
 	err := app.decodePostForm(r, &form)
@@ -75,30 +95,19 @@ func (app *application) snippetCreateNote(w http.ResponseWriter, r *http.Request
 		return
 	}
 
-	err = app.formDecoder.Decode(&form, r.PostForm)
-	if err != nil {
-		app.clientError(w, http.StatusBadRequest)
-		return
-	}
-
-	form.CheckField(validator.NotBlank(form.Title), "title", "This field cannot be blank.")
-	form.CheckField(validator.MaxChars(form.Title, 50), "title", "This field cannot be more than 50 char long.")
-	form.CheckField(validator.NotBlank(form.Content), "content", "This field cannot be blank.")
-	form.CheckField(validator.PermittedInt(form.Expires, 1, 7, 365), "expires", "This field must equal to 1, 7 or 365.")
+	form.Validate()
 
 	if !form.Valid() {
 		data := app.newTemplateData(r)
 		data.Form = form
 		app.render(w, http.StatusUnprocessableEntity, "form.tmpl", data)
-		return
 	}
+
 	id, err := app.snippets.Insert(form.Title, form.Content, form.Expires)
 	if err != nil {
 		app.serverError(w, err)
 	}
 
-	// Use the Put() method to add a string value ("Snippet successfully
-	// created!") and the corresponding key ("flash") to the session data.
 	app.sessionManager.Put(r.Context(), "flash", "Note successfully created!")
 
 	http.Redirect(w, r, fmt.Sprintf("/snippet/%d", id), http.StatusSeeOther)
